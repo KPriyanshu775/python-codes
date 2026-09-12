2c3d08f8ae22d7dccf7b939479a149aa01b644cb
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-12 19:51:36 +0530

    practicing pandas so far

--- pandas_basics.py
+++ pandas_basics.py
@@ -10,3 +10,22 @@ temperature = pd.Series(
     index = ["Sunday","Monday","Thursday","Friday","Saturday"]
 )
 print(temperature)
+
+#  Practicing the pandas series indexing
+# First
+import pandas as pd
+s = pd.Series(
+    [10,20,30,40,50],
+    index = ["A","B","C","D","E"]
+)
+s["A"]
+s["C"]
+
+# Second problem
+
+
+
+
+
+
+
