f0c64963b30c44f018d82b618a6e89e91a95d5fa
Priyanshu Kumar <poddarpriyanshu308@gmail.com>
2026-09-12 16:26:19 +0530

    made changes

--- pandas_basics.py
+++ pandas_basics.py
@@ -3,3 +3,10 @@ s = pd.Series([10,20,30,40],
               index=['a','b','c','d'],
               name='Marks')
 print(s)
+
+import pandas as pd
+temperature = pd.Series(
+    [10,20,30,40,50],
+    index = ["Sunday","Monday","Thursday","Friday","Saturday"]
+)
+print(temperature)
